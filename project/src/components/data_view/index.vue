0b3742d7de0b83ec8284068ede17cbc8f682bdcb
大幅 <527431542@qq.com>
2020-04-28 21:10:15 +0800

    update 统计图

--- project/src/components/data_view/index.vue
+++ project/src/components/data_view/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="myChart" style="width：600px;height:650px">
-  </div>
+  <div id="myChart" style="width：600px;height:650px"></div>
 </template>
 
 
@@ -9,51 +8,10 @@ import Echarts from "echarts";
 export default {
   data() {
     return {
-      // mockData: [
-      //   {
-      //     goodsId: 1,
-      //     num: 10
-      //   },
-      //   {
-      //     goodsId: 2,
-      //     num: 20
-      //   },
-      //   {
-      //     goodsId: 3,
-      //     num: 30
-      //   },
-      //   {
-      //     goodsId: 4,
-      //     num: 40
-      //   }
-      // ],
       options: {
-        // title: {
-        //   text: "销售量前十的产品统计图"
-        // },
-        // tooltip: {}, //提示框
-        // xAxis: {
-        //   //横坐标的值
-        //   data: [],
-        //   name: "产品类别名"
-        // },
-        // yAxis: {},
-        // series: [
-        //   {
-        //     //纵坐标的值
-        //     name: "销售量",
-        //     type: "bar",
-        //     data: [],
-        //     itemStyle: {
-        //       normal: {
-        //         color: "rgb(145,199,174)"
-        //       }
-        //     }
-        //   }
-        // ]
 
         title: [
-          // #1  折现图名称
+          // #1  折线图名称
           {
             text: "每天的成交量统计图",
             x: "0%",
@@ -83,15 +41,15 @@ export default {
             left: "5%",
             right: "10%",
             top: "10%",
-            bottom: "55%",
+            bottom: "50%",
             containLabel: true
           },
           {
             gridindex: 1,
             left: "5%",
             right: "10%",
-            top: "50%",
-            bottom: "10%",
+            top: "62%",
+            bottom: "0%",
             containLabel: true
           }
         ],
@@ -133,7 +91,7 @@ export default {
           {
             xAxisIndex: 0,
             yAxisIndex: 0,
-            name: "日期",
+            name: "成交量",
             type: "line",
             smooth: true,
             label: {
@@ -146,7 +104,7 @@ export default {
           {
             xAxisIndex: 1,
             yAxisIndex: 1,
-            name: "产品名",
+            name: "销售量",
             type: "bar",
             itemStyle: {
               normal: {
@@ -173,84 +131,46 @@ export default {
       this.fetchData(myChart);
       // this.fetchMock(myChart)
     },
-    // fetchMock(myChart) {
-    //   setTimeout(() => {
-    //     myChart.hideLoading();
-    //     myChart.setOption({
-    //       xAxis: [
-    //         {
-    //           data: [1,2,3,4]
-    //         }
-    //       ],
-    //       series: [
-    //         {
-    //           data: [10,20,30,40]
-    //         }
-    //       ]
-    //     })
-    //   }, 500)
-    // },
+
     fetchData(myChart) {
       this.$axios
         .post("/FurnitureAdm/SaleView")
         .then(res => {
-          //console.log(res)
+          // console.log(res);
           let goodsName = res.data.map(item => {
             return item.goodsName;
           });
           let num = res.data.map(item => {
             return item.num;
           });
-          let orderDate = res.data.map(item => {
-            return item.orderDate;
+
+          let orderDate = [];
+          res.data.forEach(item => {
+            if (item.orderDate) {
+              orderDate.push(item.orderDate);
+              return item.orderDate;
+            }
           });
-          let totalNum = res.data.map(item => {
-            return item.totalNum;
+          // console.log(orderDate);
+          let totalNum = [];
+          res.data.forEach(item => {
+            if (item.totalNum) {
+              totalNum.push(item.totalNum);
+              return item.totalNum;
+            }
           });
+          // console.log(totalNum);
           setTimeout(() => {
             // 获取到数据后隐藏加载动画
             myChart.hideLoading();
             // console.log(res.data);
             myChart.setOption({
-              // xAxis: [
-              //   {
-              //     data: goodsName.slice(0,10), //将异步请求获取到的数据进行装载
-              //     axisLabel: {
-              //         interval: 0,
-              //         rotate: 30
-              //   },
-              //   }],
-              grid: [
-                {
-                  left: "5%",
-                  right: "10%",
-                  top: "10%",
-                  bottom: "50%",
-                  containLabel: true
-                },
-                {
-                  gridindex: 1,
-                  left: "5%",
-                  right: "10%",
-                  top: "62%",
-                  bottom: "0%",
-                  containLabel: true
-                }
-              ],
-
+              //将异步请求获取到的数据进行装载
               xAxis: [
                 {
-                  gridIndex: 0,
-                  type: "category",
-                  name: "日期",
-                  boundaryGap: false,
-                  data: orderDate,
+                  data: orderDate 
                 },
                 {
-                  gridIndex: 1,
-                  name: "\n\n 产品名",
-                  type: "category",
-                  boundaryGap: true,
                   data: goodsName.slice(0, 10),
                   axisLabel: {
                     interval: 0,
@@ -258,58 +178,14 @@ export default {
                   }
                 }
               ],
-              yAxis: [
-                {
-                  gridIndex: 0,
-                  type: "value",
-                  name: "成交量"
-                  //axisLabel: {
-                  //formatter: '{value} %'
-                  //},
-                },
-                {
-                  gridIndex: 1,
-                  type: "value",
-                  name: "销售量"
-                  //axisLabel: {
-                  //    formatter: '{value} ℃'
-                  //},
-                }
-              ],
               series: [
                 {
-                  xAxisIndex: 0,
-                  yAxisIndex: 0,
-                  date:totalNum,
-                  name: "日期",
-                  type: "line",
-                  smooth: true,
-                  label: {
-                    normal: {
-                      show: true,
-                      position: "right"
-                    }
-                  }
+                  data: totalNum
                 },
                 {
-                  xAxisIndex: 1,
-                  yAxisIndex: 1,
-                  data:num.slice(0, 10),
-                  name: "产品名",
-                  type: "bar",
-                  itemStyle: {
-                    normal: {
-                      color: "#4ea397"
-                    }
-                  }
+                  data: num.slice(0, 10)
                 }
               ]
-
-              // series: [
-              //   {
-              //     data: num.slice(0, 10)
-              //   }
-              // ]
             });
           }, 500); //加载动画时长为0.5秒
         })
